cd116ca5afdabeec54c361d6ca33852caf78daed
JuhQ <juha@bin.fi>
2016-07-05 13:01:07 +0300

    Removed extra whitespace, refactored error string generation code to be more readable

--- ejs.js
+++ ejs.js
@@ -6,11 +6,11 @@ var Error = function() {
       _errorData(e);
     });
   }
-  
+
   function _errorData(e) {
     var filename = e.filename.lastIndexOf('/');
     var datetime = new Date();
-    
+
     var data = {
       type: e.type,
       path: e.filename,
@@ -20,27 +20,29 @@ var Error = function() {
       error: e.message,
       datetime: datetime
     }
-    
+
     _formatError(data);
   }
-  
+
   function _formatError(errorInfo) {
     var i = errorInfo;
     var helpPath = encodeURI("https://stackoverflow.com/search?q=" + i.error.split(' ').join('+'));
-    
-    var str = "%cType: %c" + i.type
-            + "\n%cError: %c" + i.error
-            + "\n%cFile Name: %c" + i.filename
-            + "\n%cPath: %c" + i.path
-            + "\n%cLine: %c" + i.line
-            + "\n%cColumn: %c" + i.column
-            + "\n%cDate: %c" + i.datetime
-            + "\n%cDebug : %c" + i.path + ':' + i.line
-            + "\n%cGet Help: " + "%c" + helpPath;
-  
+
+    var str = [
+      "%cType: %c" + i.type,
+      "%cError: %c" + i.error,
+      "%cFile Name: %c" + i.filename,
+      "%cPath: %c" + i.path,
+      "%cLine: %c" + i.line,
+      "%cColumn: %c" + i.column,
+      "%cDate: %c" + i.datetime,
+      "%cDebug : %c" + i.path + ':' + i.line,
+      "%cGet Help: " + "%c" + helpPath
+    ].join("\n");
+
     console.log(str, "font-weight: bold;", "color: #e74c3c;", "font-weight: bold;", "font-weight: normal; color: #e74c3c;", "font-weight: bold;", "font-weight: normal;", "font-weight: bold;", "font-weight: normal;", "font-weight: bold;", "font-weight: normal;", "font-weight: bold;", "font-weight: normal;", "font-weight: bold;", "font-weight: normal;", "font-weight: bold;", "font-weight: normal;", "font-weight: bold;", "font-weight: normal; color: #3498db;");
   }
-  
+
   return {
     listener: listener
   }
